67e95ca02c3d8b09e90b0b1c04abb1282d9fc43f
blabla9111 <dinaragindullinaa@gmail.com>
2024-05-24 11:06:26 +0300

    cleaned code app

--- app/main.py
+++ app/main.py
@@ -6,70 +6,69 @@ from threading import Thread
 from config import *
 
 
-
 class App(TKMT.ThemedTKinterFrame):
-        def __init__(self, theme, mode, usecommandlineargs=True, usethemeconfigfile=True):
-                super().__init__(APP_NAME, theme, mode,
-                                 usecommandlineargs=usecommandlineargs, useconfigfile=usethemeconfigfile)
-                self.frame = self.addLabelFrame("")
-                self.label = self.frame.Label(APP_TEXT_DICT["enter_code"])
-                self.entry =  self.frame.Entry(tk.Variable)
-                self.proctor_start = 0
-                self.button = self.frame.AccentButton(APP_TEXT_DICT["enter_code_button"], self.handleSendCodeButtonClick)
-                self.run()
-                self.proctor_start = 2
-                self.close()
+    def __init__(self, theme, mode, usecommandlineargs=True, usethemeconfigfile=True):
+        super().__init__(APP_NAME, theme, mode,
+                         usecommandlineargs=usecommandlineargs, useconfigfile=usethemeconfigfile)
+        self.frame = self.addLabelFrame("")
+        self.label = self.frame.Label(APP_TEXT_DICT["enter_code"])
+        self.entry = self.frame.Entry(tk.Variable)
+        self.proctor_start = 0
+        self.button = self.frame.AccentButton(APP_TEXT_DICT["enter_code_button"], self.handleSendCodeButtonClick)
+        self.run()
+        self.proctor_start = 2
+        self.close()
 
-        def handleSendCodeButtonClick(self):
-            url = ECG_PROCTOR_SERVER_URL+FIND_CODE_URL
-            data = {REQUEST_RARAMS_DICT["code"]:self.entry.get()}
+    def handleSendCodeButtonClick(self):
+        url = ECG_PROCTOR_SERVER_URL+FIND_CODE_URL
+        data = {REQUEST_RARAMS_DICT["code"]: self.entry.get()}
+        r = requests.post(url, json=data)
+        global id_transaction
+        id_transaction = r.text
+        if r.text != "None":
+            print(r.text)
+            self.label.configure(text=APP_TEXT_DICT["start_ecg"])
+            self.entry.destroy()
+            self.button.destroy()
+            self.button = self.frame.AccentButton(APP_TEXT_DICT["start_ecg_button"], self.handleStartECGButtonClick)
+
+    def handleStartECGButtonClick(self):
+        url = ECG_PROCTOR_SERVER_URL+SEND_INIT_ECG_URL
+        ecg = take_ecg()
+        if ecg is None:  # убрали с порта
+            self.proctor_start = 2
+            self.button.destroy()
+        else:
+            data = {REQUEST_RARAMS_DICT["id_transaction"]: id_transaction,
+                    REQUEST_RARAMS_DICT["ecg_data"]: ecg}
             r = requests.post(url, json=data)
-            global id_transaction 
-            id_transaction = r.text
-            if r.text != "None":
-                print(r.text)
-                self.label.configure(text=APP_TEXT_DICT["start_ecg"])
-                self.entry.destroy()
-                self.button.destroy()
-                self.button = self.frame.AccentButton(APP_TEXT_DICT["start_ecg_button"],self.handleStartECGButtonClick)
+            self.label.configure(text=APP_TEXT_DICT["ecg_in_progress"])
+            self.button.destroy()
+        # Страрт непрерывной идентификации
+        Thread(target=self.ecg).start()
 
-        def handleStartECGButtonClick(self):
-            url = ECG_PROCTOR_SERVER_URL+SEND_INIT_ECG_URL
-            ecg = take_ecg() 
-            if ecg == None:# убрали с порта
-                  self.proctor_start = 2  
-                  self.button.destroy()
-            else:      
-                data = {REQUEST_RARAMS_DICT["id_transaction"]:id_transaction,
-                        REQUEST_RARAMS_DICT["ecg_data"]:ecg}
+    def ecg(self):
+        text = ""
+        while text != APP_END_CODE and self.proctor_start != 2:
+            time.sleep(10)
+            url = ECG_PROCTOR_SERVER_URL+CHECK_ECG_URL
+            ecg = take_ecg()
+            if ecg is None:  # убрали с порта
+                self.proctor_start = 2
+            else:
+                data = {REQUEST_RARAMS_DICT["id_transaction"]: id_transaction,
+                        REQUEST_RARAMS_DICT["ecg_data"]: ecg}
                 r = requests.post(url, json=data)
-                self.label.configure(text=APP_TEXT_DICT["ecg_in_progress"])
-                self.button.destroy()
-            # Страрт непрерывной идентификации
-            Thread(target=self.ecg).start()
+                print(r.text)
+                text = r.text
+        self.close(id_transaction)
+        self.label.configure(text=APP_TEXT_DICT["ecg_end"])
 
-        def ecg(self):
-            text = ""
-            while text!=APP_END_CODE and self.proctor_start != 2:
-                time.sleep(10)
-                url = ECG_PROCTOR_SERVER_URL+CHECK_ECG_URL
-                ecg = take_ecg() 
-                if ecg == None:# убрали с порта
-                     self.proctor_start = 2
-                else:
-                    data = {REQUEST_RARAMS_DICT["id_transaction"]:id_transaction,
-                        REQUEST_RARAMS_DICT["ecg_data"]:ecg}
-                    r = requests.post(url, json=data)
-                    print(r.text)
-                    text = r.text
-            self.close(id_transaction)
-            self.label.configure(text=APP_TEXT_DICT["ecg_end"])
+    def close(self, id_transaction="None"):
+        url = ECG_PROCTOR_SERVER_URL+CLOSE_DESKTOP_URL
+        data = {REQUEST_RARAMS_DICT["id_transaction"]: id_transaction}
+        requests.post(url, json=data)
 
-        def close(self, id_transaction = "None"):
-            url = ECG_PROCTOR_SERVER_URL+CLOSE_DESKTOP_URL
-            data = {REQUEST_RARAMS_DICT["id_transaction"]:id_transaction}
-            requests.post(url, json=data)
-             
-             
+
 if __name__ == "__main__":
-    App(APP_THEME[0],APP_THEME[1])
+    App(APP_THEME[0], APP_THEME[1])
--- app/take_ecg.py
+++ app/take_ecg.py
@@ -1,40 +1,39 @@
-import serial 
+import serial
 import serial.tools.list_ports
-import time 
+import time
 
 
 def take_ecg():
-    # ports = list(serial.tools.list_ports.comports())
-    # port_name = ""
-    # for p in ports:
-    #     port_name =p.name
-    #     print(p.name)
-    # ecg_array = list()
-    # try:
-    #     arduino = serial.Serial(port=port_name, baudrate=115200) 
-    #     print('Получаю данные из порта', arduino.port)
-    #     i = 0
-    #     while i<500:
-    #         i=i+1
-    #         try:
-    #             s = arduino.read(3)
-    #             s=s.hex()
-    #             s= s.replace("4130","")
-    #             d = int(s[0],base=16)*16+int(s[1],base=16)
-    #             ecg_array.append(d)
-    #         except Exception as e:
-    #             print(e.with_traceback)
-    #             return None
-    #         time.sleep(0.01)
-    # except Exception:
-    #     print("Ошибка при попытке получения данных")
-    #     return None
-    
-    # ecg = str(ecg_array).replace("[","").replace("]","").replace(",","")
-    # return ecg
+    ports = list(serial.tools.list_ports.comports())
+    port_name = ""
+    for p in ports:
+        port_name = p.name
+    ecg_array = list()
+    try:
+        arduino = serial.Serial(port=port_name, baudrate=115200)
+        print('Получаю данные из порта', arduino.port)
+        i = 0
+        while i < 500:
+            i = i+1
+            try:
+                s = arduino.read(3)
+                s = s.hex()
+                s = s.replace("4130", "")
+                d = int(s[0], base=16)*16+int(s[1], base=16)
+                ecg_array.append(d)
+            except Exception as e:
+                print(e.with_traceback)
+                return None
+            time.sleep(0.01)
+    except Exception:
+        print("Ошибка при попытке получения данных")
+        return None
 
-    l = list()
-    for i in range(0,500):
-        l.append(i)
-    l_2 = str(l).replace("[","").replace("]","").replace(",","")
-    return l_2
+    ecg = str(ecg_array).replace("[", "").replace("]", "").replace(",", "")
+    return ecg
+# если нет датчика, то можно для тестирования включить заглушку
+    # l = list()
+    # for i in range(0,500):
+    #     l.append(i)
+    # l_2 = str(l).replace("[","").replace("]","").replace(",","")
+    # return l_2
